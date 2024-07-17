6071b3d617fa8507b8c31cd9a0586a7c9363c8d6
Nikki Tiwari <nikkitiwari915@gmail.com>
2024-07-17 14:05:55 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,22 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+const App = () => {
+  const [name, setName] = useState("");
+  const onChangeEvent = (event) => {
+    setName(`Hello ${event.target.value}!`);
+  };
 
-e
+  return (
+    <div>
+      {
+        <>
+          <input type="{text}" onChange={onChangeEvent} />
+          <p className="display">{name}</p>
+        </>
+      }
+    </div>
+  );
+};
+
+export default App;
