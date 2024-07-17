4422c45c5d5ec19dfb6eae47d3ed17faa9cb697b
Nikki Tiwari <nikkitiwari915@gmail.com>
2024-07-17 13:38:19 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,4 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
-
-export default App
+e
